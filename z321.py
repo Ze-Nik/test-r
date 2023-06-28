a581ba6f6386b06ffbc453e93b0759cad8e7c6c2
ZEMSKOV <nekit_zemskov@mail.ru>
2023-06-28 22:02:36 +0700

    модуль 3 часть 2 уровень 1

---
+++ z321.py
@@ -0,0 +1,11 @@
+sp = [1, 4, 1, 6, "hello", "a", 5, "hello"]
+k = 0
+spp = []
+for i in range(len(sp)):
+    for j in range(len(sp)):
+        if sp[i] == sp[j]:
+            k += 1
+    if k > 1:
+        spp.append(sp[i])
+    k = 0
+print(set(spp))

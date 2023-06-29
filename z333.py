5cc746a6fbbf31b41ba0e23b5f5300d6ef2e3890
ZEMSKOV <nekit_zemskov@mail.ru>
2023-06-29 20:50:21 +0700

    модуль 3 часть 3 уровень 3

---
+++ z333.py
@@ -0,0 +1,23 @@
+m = [56, 9, 11, 2]
+k1 = 0
+k2 = 0
+ch = ''
+for i in range(len(m) - 1):
+    for j in range(len(m) - i - 1):
+        k1 = len(str(m[j]))
+        if k1 == 1:
+            d1 = 1
+        else:
+            d1 = 10 * (k1 - 1)
+        k2 = len(str(m[j + 1]))
+        if k2 == 1:
+            d2 = 1
+        else:
+            d2 = 10 * (k2 - 1)
+        if (int(m[j]) // d1) < (int(m[j + 1]) // d2):
+            m[j], m[j + 1] = m[j + 1], m[j]
+for i in range(len(m)):
+    ch = ch + str(m[i])
+print(ch)
+
+

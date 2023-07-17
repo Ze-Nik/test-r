86be487fa97fa0249bb0c8c22fcbfc278f3f445e
ZEMSKOV <nekit_zemskov@mail.ru>
2023-07-17 20:44:22 +0700

    модуль 9

---
+++ test-env/shop/main/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path('index', views.index, name="Index"),
+    path('info', views.info, name="Info"),
+    path('product', views.product, name="product")
+]

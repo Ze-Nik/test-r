7804b723ee1df31f0eefb7adc27330f3df044961
Ze-Nik <130284188+Ze-Nik@users.noreply.github.com>
2023-07-17 20:50:52 +0700

    Delete site/com/main directory

--- site/com/main/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-from . import views
-
-urlpatterns = [
-    path('titul_page', views.titul_page, name='Титульная страница'),
-    path('user_page', views.user_page, name='Страница пользователя'),
-    path('reg_page', views.reg_page, name='Страница регистрации'),
-    path('auto_page', views.auto_page, name='Страница авторизации')
-]

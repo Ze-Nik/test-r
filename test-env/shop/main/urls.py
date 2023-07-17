3689b2661355fc17742d96c66c66bc6890dc83dc
Ze-Nik <130284188+Ze-Nik@users.noreply.github.com>
2023-07-17 20:51:02 +0700

    Delete test-env/shop/main directory

--- test-env/shop/main/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-from . import views
-
-urlpatterns = [
-    path('index', views.index, name="Index"),
-    path('info', views.info, name="Info"),
-    path('product', views.product, name="product")
-]
